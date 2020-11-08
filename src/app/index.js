ee7eac7881e9797c98c1cda06a161d4d0cfecbad
MD. Towhidul Islam <tuhintowhidul9@gmail.com>
2020-11-08 21:59:01 +0600

    #6 - Cycling Through Data

--- src/app/index.js
+++ src/app/index.js
@@ -5,21 +5,20 @@ var ReactDOM = require('react-dom');
 var TodoComponent = React.createClass({
     getInitialState: function(){
         return {
-            todos: ['wash up', 'eat some cheese', 'take a nap'],
-            age:30
+            todos: ['wash up', 'eat some cheese', 'take a nap']
         }
     }, //getInitialState
     render: function(){
-        setTimeout(function(){this.setState({age:35})}.bind(this),5000);
+        var todos = this.state.todos;
+        todos = todos.map(function(item, index){
+            return(
+                <li>{item}</li>
+            );
+        });
         return(
             <div id="todo-list">
-                age:{this.state.age}
                 <p>The busiest people have the most leisure...</p>
-                <ul>
-                    <li>{this.state.todos[0]}</li>
-                    <li>{this.state.todos[1]}</li>
-                    <li>{this.state.todos[2]}</li>
-                </ul>
+                <ul>{todos}</ul>
             </div>
         );
     } //render
